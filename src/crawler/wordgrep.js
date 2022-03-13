b74ed47e77843efd26ab08f99ca6ce3e517321a1
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2022-03-13 18:04:01 +0100

    less logs readability improvements

--- src/crawler/wordgrep.js
+++ src/crawler/wordgrep.js
@@ -1,5 +1,4 @@
-let fetch = require('node-fetch'),
-    log = console.log
+let fetch = require('node-fetch'), log = console.log
 
 const fetchData = function (url) {
     return new Promise(async (fulfill, reject) => {
@@ -17,62 +16,37 @@ const dateStrToIso = function (dateStr) {
     return new Date(dateStr).toISOString();
 }
 
+const getBasePath = function (url) {
+    return url + '/wp-json/wp/v2/';
+}
+
+const getPosts = function (url, items = 100, page) {
+    const basePath = this.getBasePath(url);
+    const postUrl = basePath + 'posts?per_page=' + items
+    return fetchData(postUrl).then((data = []) => {
+        return data.map((p) => {
+            let {
+                id, date, modified, slug, title, status, type, link, excerpt, tags, author
+            } = p;
+
+            let lang = 'de'
+            if (link.includes('/en/')) {
+                lang = 'en';
+            }
+            date = dateStrToIso(date)
+            modified = dateStrToIso(modified)
+
+            title = (title || {}).rendered || '-1';
+            excerpt = (excerpt || {}).rendered || '-1';
+
+            return {
+                lang, id, date, title, modified, slug, status, type, link, excerpt, tags, author
+            }
+        });
+    })
+}
+
 module.exports = {
-    getBasePath: function (url) {
-        return url + /wp-json/;
-    },
-    getPosts: function (url) {
-        const basePath = this.getBasePath(url);
-        const postUrl = basePath + 'wp/v2/posts?per_page=100'
-        return fetchData(postUrl).then((data = []) => {
-            log('POSTS FOUND: ' + data.length);
-            return data.map((p) => {
-                let {
-                    id,
-                    date,
-                    // date_gmt,
-                    modified,
-                    // modified_gmt,
-                    slug,
-                    title,
-                    status,
-                    type,
-                    link,
-                    excerpt,
-                    tags,
-                    author
-                } = p;
-
-                let lang = 'de'
-                if (link.includes('/en/')) {
-                    lang = 'en';
-                }
-                date = dateStrToIso(date)
-                modified = dateStrToIso(modified)
-
-                title = (title || {}).rendered || '-1';
-                excerpt = (excerpt || {}).rendered || '-1';
-
-                return {
-                    lang,
-                    id,
-                    date,
-                    // date_gmt,
-                    title,
-                    modified,
-                    // modified_gmt,
-                    slug,
-                    status,
-                    type,
-                    link,
-                    excerpt,
-                    tags,
-                    author
-                }
-            }).map((p) => {
-                log(p.id + ' - ' + p.lang + ' - ' + p.title.rendered);
-                return p;
-            })
-        })
-    }
+    getBasePath,
+    getPosts
 }
