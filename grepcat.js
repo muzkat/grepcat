e5a7fbc48c9d744e5174659e7ca27d9d58a57b46
hardwaresoftwork <760988+hardwaresoftwork@users.noreply.github.com>
2022-04-26 20:51:59 +0200

    url probe feature added

--- grepcat.js
+++ grepcat.js
@@ -1,9 +1,9 @@
 const urlgrep = require('./src/crawler/urlgrep'),
     pusher = require('./src/pusher/pusher'),
-    wordgrep = require('./src/crawler/wordgrep')
+    wordgrep = require('./src/crawler/wordgrep'),
+    probe = require('./src/crawler/probe')
 
 module.exports = {
     crawler: urlgrep,
-    pusher: pusher,
-    wordgrep: wordgrep
+    pusher, wordgrep, probe
 }
---
+++ src/crawler/probe.js
@@ -0,0 +1,50 @@
+let fetch = require('node-fetch');
+
+/**
+ *
+ * @param url
+ * @param options
+ * @returns {Promise<{headers: {}, status}>}
+ *
+ * example response
+ *  *
+ * {
+ *   status: 200,
+ *   headers: {
+ *     'accept-ranges': 'bytes',
+ *     connection: 'close',
+ *     'content-length': '2373149',
+ *     'content-type': 'application/octet-stream',
+ *      server: 'nginx'
+ *   }
+ * }
+ *
+ */
+
+const probeUrl = async function (url, options) {
+    const result = await fetch(url, options);
+    const {status, headers} = result;
+    let obj = {};
+    if (headers) {
+        headers.forEach((value, name) => {
+            obj[name] = value;
+        })
+    }
+    return {
+        status,
+        headers: obj
+    }
+}
+
+// inspired from https://stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+const format = function (s, params) {
+    const args = Array.from(arguments).slice(1);
+    return s.replace(/{(\d+)}/g, function (match, index) {
+        return typeof args[index] == 'undefined' ? match : args[index];
+    });
+};
+
+module.exports = {
+    probeUrl,
+    format
+}
